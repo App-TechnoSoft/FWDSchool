c3f2ddbfc3b12759f29f10cbb8697265a52f619b
Punamthapa24 <punamthapamagar24@gmail.com>
2025-04-03 09:07:19 +0545

    modify home controller

--- FWDSchool.Web/Controllers/HomeController.cs
+++ FWDSchool.Web/Controllers/HomeController.cs
@@ -13,11 +13,11 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+     public IActionResult Index()
     {
         return View();
     }
-
+    
     public IActionResult Privacy()
     {
         return View();
@@ -28,4 +28,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
 }
