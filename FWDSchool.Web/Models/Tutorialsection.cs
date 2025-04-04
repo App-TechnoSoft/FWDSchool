805f35fb0bbca2a398d8dc5f962b0f47e0f1ce17
laxme291 <laxmigharti309@gmail.com>
2025-04-04 10:12:26 +0545

    give imagepath

--- FWDSchool.Web/Models/Tutorialsection.cs
+++ FWDSchool.Web/Models/Tutorialsection.cs
@@ -3,7 +3,7 @@ public class TutorialSection
     public int Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
-    public byte[] Image { get; set; }
+    public string ImagePath { get; set; }
     public DateTime LastUpdated { get; set; }
     public string Author { get; set; }
     public int TutorialId { get; set; }
