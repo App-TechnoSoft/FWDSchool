04fcbe25530666e6a11ecc6ee4e0144fa548c706
Punamthapa24 <punamthapamagar24@gmail.com>
2025-04-04 10:06:45 +0545

    modify string and published

--- FWDSchool.Web/Models/Tutorial.cs
+++ FWDSchool.Web/Models/Tutorial.cs
@@ -1,10 +1,10 @@
 public class Tutorial
 {
     public int Id {get; set;}
-    public String Title {get; set;}
-    public String Description {get; set;}
-    public String Type {get; set;}
-    public long Published {get; set;}
-    public String Author {get; set;}
-    
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public string Type {get; set;}
+    public DateTime Published {get; set;}
+    public string Author {get; set;}
+
 }
