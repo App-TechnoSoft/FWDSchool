86a27ef502ec70c62a40b018c6e954145a0d3e42
laxme291 <laxmigharti309@gmail.com>
2025-04-04 10:10:33 +0545

    modify tutorialsection file

--- FWDSchool.Web/Models/Tutorialsection.cs
+++ FWDSchool.Web/Models/Tutorialsection.cs
@@ -1,10 +1,10 @@
 public class TutorialSection
 {
-    public int ID { get; set; }
+    public int Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
     public byte[] Image { get; set; }
     public DateTime LastUpdated { get; set; }
     public string Author { get; set; }
-    public int TutorialID { get; set; }
+    public int TutorialId { get; set; }
 }
