a6fefb95caf492c8018e0aae4af51ea6cc0d292c
Samikshya-KC <samikshyakc021@gmail.com>
2025-04-04 10:25:50 +0545

    fix datatype

--- FWDSchool.Web/Models/Student.cs
+++ FWDSchool.Web/Models/Student.cs
@@ -1,14 +1,15 @@
 using System.Reflection.Metadata;
 
-public class Student{
-    public int Id { get; set; } 
-    public int RN {get;set;}
-    public string Name {get;set;}
-    public string Address {get;set;}
-    public string Program{get;set;}
-    public int Phone {get;set;}
-    public string Email{get;set;}
-    public string FName{get;set;}
-    public string MName{get;set;}
+public class Student
+{
+    public int Id { get; set; }
+    public string Serial { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Faculty { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string FName { get; set; }
+    public string MName { get; set; }
 
 }
