db9567cedb4e9066465e1006c4c828e981247992
Punamthapa24 <punamthapamagar24@gmail.com>
2025-04-04 10:02:32 +0545

    Add tutorial model file

---
+++ FWDSchool.Web/Models/Tutorial.cs
@@ -0,0 +1,10 @@
+public class Tutorial
+{
+    public int Id {get; set;}
+    public String Title {get; set;}
+    public String Description {get; set;}
+    public String Type {get; set;}
+    public long Published {get; set;}
+    public String Author {get; set;}
+    
+}
