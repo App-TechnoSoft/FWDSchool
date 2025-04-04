367ee0781c163ae6d8ce612d99f53a0937e8e922
Samikshya-KC <samikshyakc021@gmail.com>
2025-04-04 10:11:27 +0545

    Create student model file

---
+++ FWDSchool.Web/Models/Student.cs
@@ -0,0 +1,14 @@
+using System.Reflection.Metadata;
+
+public class Student{
+    public int Id { get; set; } 
+    public int RN {get;set;}
+    public string Name {get;set;}
+    public string Address {get;set;}
+    public string Program{get;set;}
+    public int Phone {get;set;}
+    public string Email{get;set;}
+    public string FName{get;set;}
+    public string MName{get;set;}
+
+}
