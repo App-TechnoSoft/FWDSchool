95631264004814e4af88902dbcdb9278b62c3a7f
Bishnu Rawal <rawal.bishnu@gmail.com>
2025-04-07 09:38:57 +0545

    Add new migration for tutorial type

--- FWDSchool.Web/Data/Migrations/20250407034424_TutorialTypeToEnum.Designer.cs
+++ FWDSchool.Web/Data/Migrations/20250407034424_TutorialTypeToEnum.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FWDSchool.Web.Data.Migrations
 {
     [DbContext(typeof(FWDSchoolDb))]
-    partial class FWDSchoolDbModelSnapshot : ModelSnapshot
+    [Migration("20250407034424_TutorialTypeToEnum")]
+    partial class TutorialTypeToEnum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FWDSchool.Web/Data/Migrations/20250407034424_TutorialTypeToEnum.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FWDSchool.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TutorialTypeToEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Tutorials",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Tutorials",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
