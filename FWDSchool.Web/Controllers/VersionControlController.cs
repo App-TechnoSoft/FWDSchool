5750f7cd8416d1a3c0f9f056e63faddc80517c25
Samikshya-KC <samikshyakc021@gmail.com>
2025-04-07 07:24:10 +0545

    fetch tutorials from db and render them using VersionControl

--- FWDSchool.Web/Controllers/VersionControlController.cs
+++ FWDSchool.Web/Controllers/VersionControlController.cs
@@ -5,6 +5,13 @@ public class VersionControlController : Controller
     
     public IActionResult Index()
     {
+        var db = new FWDSchoolDb();
+        var tutorials = db.Tutorials.Where(x => x.Type == "VersionControl").ToList();  // Select * from Tutorials where condition is applied
+        return View(tutorials);
+
+    }
+    public IActionResult About()
+    {
         return View();
     }
 
