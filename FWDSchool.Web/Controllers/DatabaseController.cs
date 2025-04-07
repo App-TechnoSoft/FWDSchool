8bb8583177568a4f871ce697f1499c6f05d18e65
laxme291 <laxmigharti309@gmail.com>
2025-04-07 09:22:33 +0545

    Add TutorialType enum in database

--- FWDSchool.Web/Controllers/DatabaseController.cs
+++ FWDSchool.Web/Controllers/DatabaseController.cs
@@ -7,7 +7,7 @@ public class DatabaseController : Controller
     public IActionResult Index()
     {
         var db = new FWDSchoolDb();
-        var tutorials = db.Tutorials.Where(x => x.Type == "Database").ToList();  // Select * from Tutorials
+        var tutorials = db.Tutorials.Where(x => x.Type == TutorialType.Database).ToList();  
         return View(tutorials);
       
     }
